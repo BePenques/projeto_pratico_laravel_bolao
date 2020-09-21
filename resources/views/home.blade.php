3b991cf5bee2ee53a0102fb3d041f17f5378ed4b
Betiza Barreira <be.penques@hotmail.com>
2020-09-21 16:18:47 -0300

    74. Itens do Painel de controle

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,34 +2,61 @@
 
 @section('content')
 
-<x-page_component col=8  :page=$page>
+<x-page_component col=12  :page=$page>
 
     <x-alert :msg="session('msg'??'')" :status="session('status')" />  <!-- status: success, error or notification-->
 
-    <div class="row">
-
-        @can('users-list')
-            <div onclick="window.location='{{route('users.index')}}'" style="cursor:pointer" class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-                <div class="card-header">@lang('bolao.list',['page'=>__('bolao.user_list')])</div>
-                <div class="card-body">
-                    <p class="card-text">@lang('bolao.Manage_users')</p>
+<span id="portfolio">
+    <div class="container">
+       
+        <div class="row">
+            @can('users-list')
+            <div class="col-lg-4 col-sm-6 mb-4">
+                <div class="portfolio-item" onclick="window.location='{{route('users.index')}}'" style="cursor:pointer">
+                    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img class="img-fluid" src="assets/img/portfolio/01-thumbnail.jpg" alt="" />
+                    </a>
+                    <div class="portfolio-caption">
+                        <div class="portfolio-caption-heading">@lang('bolao.list',['page'=>__('bolao.user_list')])</div>
+                        <div class="portfolio-caption-subheading text-muted">@lang('bolao.Manage_users')</div>
+                    </div>
                 </div>
-            </div>
-        @endcan
-
-        <div onclick="window.location='{{route('roles.index')}}'" style="cursor:pointer; margin-left:10px" class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-            <div class="card-header">@lang('bolao.list',['page'=>__('bolao.Role_list')])</div>
-            <div class="card-body">
-                <p class="card-text">@lang('bolao.Manage_roles')</p>
-            </div>
+            </div> 
+            @endcan 
+            <div class="col-lg-4 col-sm-6 mb-4">
+                <div class="portfolio-item" onclick="window.location='{{route('roles.index')}}'" style="cursor:pointer">
+                    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img class="img-fluid" src="assets/img/portfolio/01-thumbnail.jpg" alt="" />
+                    </a>
+                    <div class="portfolio-caption">
+                        <div class="portfolio-caption-heading">@lang('bolao.list',['page'=>__('bolao.Role_list')])</div>
+                        <div class="portfolio-caption-subheading text-muted">@lang('bolao.Manage_roles')</div>
+                    </div>
+                </div>
+            </div>  
+            <div class="col-lg-4 col-sm-6 mb-4">
+                <div class="portfolio-item" onclick="window.location='{{route('permissions.index')}}'" style="cursor:pointer">
+                    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img class="img-fluid" src="assets/img/portfolio/01-thumbnail.jpg" alt="" />
+                    </a>
+                    <div class="portfolio-caption">
+                        <div class="portfolio-caption-heading">@lang('bolao.list',['page'=>__('bolao.permission_list')])</div>
+                        <div class="portfolio-caption-subheading text-muted">@lang('bolao.Manage_permissions')</div>
+                    </div>
+                </div>
+            </div>       
         </div>
-        <div onclick="window.location='{{route('permissions.index')}}'" style="cursor:pointer; margin-left:10px" class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-            <div class="card-header">@lang('bolao.list',['page'=>__('bolao.permission_list')])</div>
-            <div class="card-body">
-                <p class="card-text">@lang('bolao.Manage_permissions')</p>
-            </div>
-        </div>               
     </div>
+</span>
 
 </x-page_component>
 
