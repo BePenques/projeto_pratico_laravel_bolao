45db7c1f2b72e016fe933ff20531b83a21bba118
Betiza Barreira <be.penques@hotmail.com>
2020-09-23 22:32:52 -0300

    83. Model e Migrate de Bolão

---
+++ app/Betting.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Betting extends Model
+{
+    protected $fillable = [
+        'user_id',//usuario_id
+        'title',//titulo
+        'current_round',//rodadaAtual
+        'score_points',//pontosResultado
+        'extra_points',//pontosExtras
+        'rate_points'//pontosTaxa
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
+
---
+++ database/migrations/2020_09_23_232448_create_bettings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bettings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('title');
+            $table->integer('current_round')->unsigned()->default(0);
+            $table->double('score_points', 8, 2);
+            $table->double('extra_points', 8, 2);
+            $table->double('rate_points', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bettings');
+    }
+}
